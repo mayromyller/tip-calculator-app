990bd6b29bf5a02d39cfac37fdbe62929293782c
Mayro Myller <deehmyller11@gmail.com>
2021-09-03 15:26:44 -0300

    chore: add colors theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  tip: {
+    white: "#FFFFFF",
+    lightCyan: "#F4FAFA",
+    lightCyan2: "#C5E4E7",
+    darkCyan: "#7F9C9F",
+    darkCyan2: "#5E7A7D",
+    cyan: "#26C2AD",
+    veryDark: "#00474B"
+  }
+}
+
+const theme = extendTheme({colors})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
